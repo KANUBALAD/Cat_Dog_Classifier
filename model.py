89e540349000a5c2fe36c1e41fad3a9a56b3216f
[Kanubala] <[dormah.d.kanubala@aims-senegal.org]>
2020-01-18 22:31:00 +0000

    Model Complexity

--- model.py
+++ model.py
@@ -15,8 +15,11 @@ class CNN(nn.Module):
         self.n_feature = n_feature
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_feature, kernel_size=5)
         self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
-        self.fc1 = nn.Linear(n_feature*53*53, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv3 = nn.Conv2d(n_feature,n_feature, kernel_size=5)
+        self.conv4 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
+        self.conv5 = nn.Conv2d(n_feature,n_feature, kernel_size=5)
+        self.fc1 = nn.Linear(n_feature*3*3, 50)
+        self.fc2 = nn.Linear(50, 2)
         
     def forward(self, x, verbose=False):
         x = self.conv1(x)
@@ -25,9 +28,19 @@ class CNN(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)
-        x = x.view(-1, self.n_feature*53*53)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, self.n_feature*3*3)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
-        return x
+        return x    
+  
